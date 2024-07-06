a462ee72781686eaa0828c8d115f8255f21ffccb
Akhil Aravindakshan <akhil.dev.00782@gmail.com>
2024-07-06 17:07:18 -0300

    Auto close sidebar on click

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 import { usePathname } from "next/navigation";
@@ -38,23 +38,26 @@ const MobileNav = () => {
       <SheetContent className="flex - flex-col">
         {/* logo */}
         <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
-            <h1 className=" text-4xl font-semibold">
-              Akhil<span className="text-accent">.</span>
-            </h1>
-          </Link>
+          <SheetClose asChild>
+            <Link href="/">
+              <h1 className=" text-4xl font-semibold">
+                Akhil<span className="text-accent">.</span>
+              </h1>
+            </Link>
+          </SheetClose>
         </div>
         {/* nav */}
         <nav className="flex flex-col gap-8 justify-center items-center">
           {links.map((link, i) => {
             return (
-              <Link
-                key={i}
-                href={link.href}
-                className={`${pathname === link.href && "text-accent border-b-2 border-accent"} capitalize text-xl hover:text-accent transition-all `}
-              >
-                {link.name}
-              </Link>
+              <SheetClose asChild key={i}>
+                <Link
+                  href={link.href}
+                  className={`${pathname === link.href && "text-accent border-b-2 border-accent"} capitalize text-xl hover:text-accent transition-all `}
+                >
+                  {link.name}
+                </Link>
+              </SheetClose>
             );
           })}
         </nav>
