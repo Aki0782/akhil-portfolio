241380c43976873a171199c152012544e7ab86da
Akhil Aravindakshan <akhil.dev.00782@gmail.com>
2024-07-08 16:09:40 -0300

    Work page

---
+++ src/components/WorkSliderBtns.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+import { useSwiper } from "swiper/react";
+
+type WorkSliderBtnsProps = {
+  containerStyles: string;
+  iconStyles: string;
+  btnStyles: string;
+};
+const WorkSliderBtns: React.FC<WorkSliderBtnsProps> = ({
+  containerStyles,
+  iconStyles,
+  btnStyles
+}) => {
+  const swiper = useSwiper();
+
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={iconStyles} />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={iconStyles} />
+      </button>
+    </div>
+  );
+};
+
+export default WorkSliderBtns;
