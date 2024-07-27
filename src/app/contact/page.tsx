120c8a4163520d47fb1fa2d8e22b71c4c310ca63
Akhil Aravindakshan <akhil.dev.00782@gmail.com>
2024-07-27 17:56:47 -0300

    form fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -74,7 +74,7 @@ const Contact = () => {
       const sendEmail = await emailjs.sendForm(
         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
         process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
-        formRef.current,
+        formRef.current || "",
         {
           publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || ""
         }
